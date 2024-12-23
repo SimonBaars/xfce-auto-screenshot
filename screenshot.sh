4f40bfd6fe64601d41b72fe42064f95d1ead4b23
Simon <8466614+SimonBaars@users.noreply.github.com>
2024-12-23 18:48:44 +0100

    Simplify script

--- screenshot.sh
+++ screenshot.sh
@@ -2,11 +2,11 @@
 
 # Directory to save screenshots
 BASE_DIR="$HOME/Pictures/Screenshots"
-PREV_SCREENSHOT="/tmp/prev_screenshot.png"
+PREVIOUS_FILE=""
 
 # Function to take a screenshot
 take_screenshot() {
-    local timestamp=$(date +'%Y/%m/%d/%H_%M_%S_%3N')
+    local timestamp=$(date +'%Y/%m/%d/%H_%M_%S')
     local dir=$(dirname "$BASE_DIR/$timestamp.png")
     mkdir -p "$dir"
     xfce4-screenshooter -f -s "$BASE_DIR/$timestamp.png"
@@ -23,15 +23,9 @@ fi
 while true; do
     new_screenshot=$(take_screenshot)
     sleep 10
-    if [ -f "$PREV_SCREENSHOT" ]; then
-        if compare -metric AE "$PREV_SCREENSHOT" "$new_screenshot" null: 2>/dev/null; then
-            rm "$new_screenshot"
-        else
-            cp "$new_screenshot" "$PREV_SCREENSHOT"
-            pngquant --force --ext .png "$new_screenshot"
-        fi
+    if [ -f "$PREVIOUS_FILE" ] && cmp -s "$new_screenshot" "$PREVIOUS_FILE"; then
+        rm "$new_screenshot"
     else
-        cp "$new_screenshot" "$PREV_SCREENSHOT"
-        pngquant --force --ext .png "$new_screenshot"
+        PREVIOUS_FILE="$new_screenshot"
     fi
 done
